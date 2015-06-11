a3ab40a5c400a600e61fc79a6c87b5690d11b267
Deadarius <deadarius.dendr@gmail.com>
2015-06-11 20:54:57 +1200

    Finished quicksort

--- quick-sort/main.go
+++ quick-sort/main.go
@@ -4,36 +4,58 @@ import "fmt"
 import "math/rand"
 
 func main() {
-	inArray := rands.Perm(10)
+	inArray := rand.Perm(10)
 	fmt.Println("in:")
 	fmt.Printf("%v", inArray)
 	fmt.Println()
 
-	quicksort(inArray)
+	quicksort(inArray, 0, len(inArray)-1)
 	fmt.Println()
 	fmt.Println("out:")
-	fmt.Printf("%v", outArray)
+	fmt.Printf("%v", inArray)
 	fmt.Println()
 }
 
 func quicksort(arr []int, lo int, hi int) {
-  pivot := partition(arr, lo, hi)
-  quicksort(arr, lo, pivot)
-  quicksort(arr, pivot + 1, hi)
+	if lo >= hi {
+		return
+	}
+
+	pivotIndex := partition(arr, lo, hi)
+
+	quicksort(arr, lo, pivotIndex)
+	quicksort(arr, pivotIndex+1, hi)
 }
 
-func partition(arr []int, lo int, hi int) {
-  storedIndex:= lo
-  storedValue:= arr[storedIndex]
-  pivotValue:= arr[hi]
+func partition(arr []int, lo int, hi int) int {
+	pivotIndex := arr[(lo+hi)/2]
+	pivotValue := arr[pivotIndex]
 
-  for index := lo + 1;  <= hi; ++ {
-    cur := arr[index]
-    if cur < pivotValue {
-      arr[index] = storedValue
-      storedValue = arr[storedIndex] = cur
-      storedIndex = index
-    }
-  }
-  return storedIndex
+	swap(arr, pivotIndex, hi)
+
+	left := lo
+	right := hi - 1
+
+	for left < right {
+		for arr[left] < pivotValue {
+			left++
+		}
+
+		for right >= left && arr[right] >= pivotValue {
+			right--
+		}
+
+		if right > left {
+			swap(arr, left, right)
+		}
+	}
+
+	swap(arr, hi, left)
+	return left
+}
+
+func swap(arr []int, first int, second int) {
+	temp := arr[first]
+	arr[first] = arr[second]
+	arr[second] = temp
 }
